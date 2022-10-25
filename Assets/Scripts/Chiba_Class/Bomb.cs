6200f5fff61fd34a029858c2af2191394b0eca6d
chibatomo <tomokichiba0803@gmail.com>
2022-10-25 16:02:46 +0900

    10.25[追加]体温、スキャンの仮を追加UIの追加

--- Assets/Scripts/Chiba_Class/Bomb.cs
+++ Assets/Scripts/Chiba_Class/Bomb.cs
@@ -1,76 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField,Tooltip("爆弾となるゲームオブジェクトをここに")]
-    private GameObject bombobj_;
+    private GameObject bombObj_;
 
     //これが0になったら爆弾が設置される
     [SerializeField, Tooltip("爆弾設置にかかる時間")]
-    private float bombtime_ = 10.0f;
+    private float bombTime_ = 10.0f;
+
+    [SerializeField,Tooltip("時間表記用スライダー")]
+    private Slider bombSlider_;
+
+    [SerializeField, Tooltip("時間表記用スライダー")]
+    private Canvas bombCanvas_;
 
     //ボタンが押されたときにtrueになる
-    private bool installationflg_ = false;
+    private bool installationFlg_ = false;
 
     //プレイヤーが爆弾設置範囲内に入ったらtrueになる
-    private bool rangeflg_ = false;
+    private bool rangeFlg_ = false;
 
     Vector3 setPos_ = new Vector3(0.0f, -300.0f, 0.0f);
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
+    private float bombLimit_ = 0.0f;
 
-    //}
+    //連続で実行されないようにする
+    private bool flg_ = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bombCanvas_.enabled = false; // UI無効にする
+        bombSlider_.maxValue = bombTime_;
+       
+    }
 
     // Update is called once per frame
     void Update()
     {
         //Aボタン
-        if (Input.GetButton("Fire1")&& rangeflg_ == true)
+        if (Input.GetButton("Fire1")&& rangeFlg_ == true)
         {
-            installationflg_ = true;
+            installationFlg_ = true;
         }
         else
         {
-            installationflg_ = false;
+            installationFlg_ = false;
         }
 
-        //0秒になると実行
-        if(bombtime_<=0)
-        {
-            bombtime_ = 0.0f;
-            //爆弾が表示される
-            bombobj_.SetActive(true);
-            //エリアを非表示に(または削除)
-            //this.gameObject.SetActive(false);
-            this.transform.localPosition = setPos_; 
-        }
+        //角度を同じにする
+        bombCanvas_.transform.localRotation = Camera.main.transform.rotation;
     }
 
     private void FixedUpdate()
     {
-        //trueのとき1秒ずつ減っていく
-        if(installationflg_ == true)
+        //trueのとき1秒ずつ足される
+        if(installationFlg_ == true)
         {
-            bombtime_ -= Time.deltaTime;
+            bombLimit_ += Time.deltaTime;
         }
+
+        //0秒になると実行
+        if (bombTime_ <= bombLimit_ && flg_ == false)
+        {
+            bombCanvas_.enabled = false; // UI無効にする
+            bombTime_ = 10.0f;
+            //爆弾が表示される
+            Debug.Log("爆弾が置かれた");
+            bombObj_.SetActive(true);
+            //エリアを非表示に(または削除)
+            //this.gameObject.SetActive(false);
+            this.transform.localPosition = setPos_;
+            flg_ = true;
+        }
+       bombSlider_.value = bombLimit_;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            rangeflg_ = true;
+            rangeFlg_ = true;
+            bombCanvas_.enabled = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            rangeflg_ = false;
+            rangeFlg_ = false;
+            bombCanvas_.enabled = false;
         }
     }
 }
--- Assets/Scripts/Chiba_Class/Player.cs
+++ Assets/Scripts/Chiba_Class/Player.cs
@@ -1,85 +1,210 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     Rigidbody rb_;
+    MeshRenderer mesh_;
     [SerializeField,Tooltip("カメラを入れる")]
-    private Camera camerapos_;
+    private Camera cameraPos_;
 
     [SerializeField,Tooltip("プレイヤーのスピード")]
-    float moveSpeed_ = 1.0f;
+    private float moveSpeed_ = 1.0f;
 
-    private bool moveflg_ = true;
+    [SerializeField]
+    private TMP_Text tempText_;
 
+    [SerializeField]
+    private Slider tempSlider_;
+
+    [SerializeField]
+    private float temp_;
+
+    [SerializeField]
+    private float moveTemp_ = 0.3f;
+
+    [SerializeField,Tooltip("(毎秒)体温のぶれ")]
+    private Vector2 tempRandom_ = new Vector2(-0.45f, 0f);
+
+    [SerializeField,Tooltip("体温の最低値と最大値")]
+   
+    private Vector2 tempRange_ = new Vector2(37.0f, 50.0f);
+
+    
+    [SerializeField,Tooltip("ステルスの使用後にどれくらい体温が上がるか")]
+    private float tempStealth_ = 20.0f;
+
+
+    private bool moveFlg_ = true;
     //プレイヤーが爆弾設置範囲内に入ったらtrueになる
-    private bool rangeflg_ = false;
+    private bool rangeFlg_ = false;
+    //プレイヤーがスキャン行動に入ったらtrueになる
+    private bool scanFlg_ = false;
+    //スキャンに成功したらtrue
+    private bool scanSuccess_ = false;
+
+    //カメラの向きを取得する用
+    private Vector3 forward_ = new Vector3(1, 0, 1);
+
+    //元のマテリアルカラーの一時保存用
+    private Color32 mat_ = new Color32(0, 0, 0, 0);
+
+    //敵のゲームオブジェクトの取得用
+    private GameObject enemy_;
 
     void Start()
     {
         rb_ = GetComponent<Rigidbody>();
+        mesh_ = GetComponent<MeshRenderer>();
+
+        //体温の初期値
+        temp_ = Random.Range(37.0f, 39.0f);
+
+        //体温の最大値
+        tempSlider_.maxValue = tempRange_.y;
+
+        //マテリアル
+        mat_ = mesh_.material.color;
     }
 
     void Update()
     {
+        //爆弾設置
+        if (Input.GetButton("Fire1") && rangeFlg_ == true)
+        {
 
-        //inputHorizontal = Input.GetAxisRaw("Horizontal");
-        //inputVertical = Input.GetAxisRaw("Vertical");
+            moveFlg_ = false;
+        }
+        else
+        {
+            moveFlg_ = true;
+        }
+
+        //敵のスキャン(Downは仮なので後で外す)
+        if (Input.GetButtonDown("Fire2")&& moveFlg_ == true&& scanFlg_ == false&&temp_<tempRange_.y)
+        {
+            //スキャンのシステムができるまで成功フラグはここに入れとく
+            scanSuccess_ = true;
+
+            scanFlg_ = true;
+            Debug.Log("Bボタンが押された");
+        }
+        //else
+        //{
+        //    ScanFlg_ = false;
+        //}
+
+
+
     }
 
     void FixedUpdate()
     {
-        if (Gamepad.current == null && Keyboard.current == null)
-        {
-            Debug.Log("ゲームパッドかキーボードを接続してください");
-            return;
-        }
-        if (Input.GetButton("Fire1")&& rangeflg_ == true)
-        {
-           moveflg_ = false;
-        }
-        else
-        {
-            moveflg_ = true;
-        }
+        //if (Gamepad.current == null && Keyboard.current == null)
+        //{
+        //    Debug.Log("ゲームパッドかキーボードを接続してください");
+        //    return;
+        //}
 
 
-        //移動関連の関数
-        if (moveflg_ == true)
+        //移動関連の関数(体温が一定以上だと動けなくなる)
+        if (moveFlg_ == true )
         {
-            move();
+            Move();
         }
-        
 
+        //スキャンが成功したら実行される
+        if (scanSuccess_ == true )
+        {
+            StartCoroutine("Scan");
+            scanSuccess_ = false;
+        }
+
+        Temp();
         //カメラリセットを作る
-        
+
+        //UIに反映
+        tempText_.SetText(temp_.ToString("F1") + ("℃"));
+        tempSlider_.value = temp_;
+
     }
-    private void move()
+    private void Move()
     {
         Vector2 _leftStick = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        // カメラの方向から、X-Z平面の単位ベクトルを取得
-        Vector3 _cameraForward = Vector3.Scale(camerapos_.transform.forward, new Vector3(1, 0, 1)).normalized;
+        // カメラの方向から、ベクトルを取得
+        Vector3 _cameraForward = Vector3.Scale(cameraPos_.transform.forward, forward_).normalized;
 
         // 方向キーの入力値とカメラの向きから、移動方向を決定
         Vector3 _moveForward = _cameraForward * _leftStick.y + Camera.main.transform.right * _leftStick.x;
 
         
-        rb_.velocity = _moveForward * moveSpeed_ + new Vector3(0, rb_.velocity.y, 0);
+        rb_.velocity = _moveForward * moveSpeed_ + new Vector3(0, rb_.velocity.y, 0).normalized;
 
         // キャラクターの向きを進行方向に
         if (_moveForward != Vector3.zero)
         {
             transform.rotation = Quaternion.LookRotation(_moveForward);
         }
+        //スティックが入力されているときに温度が上がる
+        if (_leftStick.x != 0|| _leftStick.y != 0)
+        {
+            temp_ += moveTemp_*Time.deltaTime;
+        }
     }
-    
+
+    private void Temp()
+    {
+        temp_ += Random.Range(tempRandom_.x,tempRandom_.y) *Time.deltaTime;
+        
+        //最低体温
+        if(temp_ < tempRange_.x)
+        {
+            temp_ = tempRange_.x+0.2f;
+        }
+
+        
+    }
+
+   
+    //コルーチンわからんので没
+    IEnumerator Scan()
+    {
+        for (int i = 0; i < tempStealth_; i++)
+        {
+            mesh_.material.color -= new Color32(0, 0, 0, 10);
+            yield return new WaitForSeconds(0.1f);
+        }
+        //mesh_.material.color = new Color32(0, 0, 0, 30);
+        //ステルスの秒数
+        yield return new WaitForSeconds(5.0f);
+
+        mesh_.material.color = mat_;
+        scanFlg_ = false;
+        Debug.Log("ステルスリセット");
+        for (int k = 0; k < tempStealth_; k++)
+        {
+            //mesh_.material.color -= new Color32(0, 0, 0, 10);
+            temp_ += 1.0f;
+            yield return new WaitForSeconds(0.2f);
+        }
+        
+        //for (int i = 0; i < 255; i++)
+        //{
+        //    
+        //}
+    }
+
+  
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "BombArea")
         {
-            rangeflg_ = true;
+            rangeFlg_ = true;
         }
        
     }
@@ -87,7 +212,7 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.tag == "BombArea")
         {
-            rangeflg_ = false;
+            rangeFlg_ = false;
         }
     }
 }
