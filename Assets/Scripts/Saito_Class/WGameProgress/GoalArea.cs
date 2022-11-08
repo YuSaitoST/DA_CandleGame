20584acd5fd1fb0185a8a9e470f7f8b6388bbf47
MSI\STzin <s.t.zint@gmail.com>
2022-11-09 01:24:58 +0900

    [追加]潜水ゲーのシーンベースデータ

---
+++ Assets/Scripts/Saito_Class/WGameProgress/GoalArea.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GoalArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+
+        // ƒS[ƒ‹ˆ—
+    }
+}
