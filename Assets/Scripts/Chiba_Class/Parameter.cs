db9ea16520e173737f06d85a774ef9aff4b2c7e1
chibatomo <tomokichiba0803@gmail.com>
2022-11-08 10:57:28 +0900

    11/06[追加]パラメータの調整用スクリプトの追加(仮置きなので使えません)

---
+++ Assets/Scripts/Chiba_Class/Parameter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parameter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void plus()
+    {
+        Debug.Log("a");
+    }
+
+    void minus()
+    {
+        Debug.Log("a");
+    }
+}
