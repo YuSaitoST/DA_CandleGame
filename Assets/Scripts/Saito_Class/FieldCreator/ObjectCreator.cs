96fe51c90dcfc58a7e128629f763744f25de9ec9
MSI\STzin <s.t.zint@gmail.com>
2022-11-25 23:11:03 +0900

    [調整]敵・アイテム等の生成

--- Assets/Scripts/Saito_Class/FieldCreator/ObjectCreator.cs
+++ Assets/Scripts/Saito_Class/FieldCreator/ObjectCreator.cs
@@ -6,13 +6,16 @@ public class ObjectCreator : MonoBehaviour
     [SerializeField] GameObject[] pref_enemys_; // 敵リスト
     [SerializeField] GameObject[] pref_items_;  // アイテムリスト
 
+    [SerializeField] GameObject parent_enemy_;  // 敵の親オブジェクト
+    [SerializeField] GameObject parent_items_;  // アイテムの親オブジェクト
+
     // Start is called before the first frame update
     void Start()
     {
         try
         {
-            PrefabCreater.CreateMultiplePrefabs("InputData/EnemyData", pref_enemys_);
-            PrefabCreater.CreateMultiplePrefabs("InputData/ItemData", pref_items_);
+            PrefabCreater.CreateMultiplePrefabs("InputData/EnemyData", pref_enemys_, parent_enemy_);
+            PrefabCreater.CreateMultiplePrefabs("InputData/ItemData", pref_items_, parent_items_);
 
         }catch(Exception e)
         {
--- Assets/Scripts/Saito_Class/StandardTool/PrefabCreater.cs
+++ Assets/Scripts/Saito_Class/StandardTool/PrefabCreater.cs
@@ -1,6 +1,13 @@
+using System;
 using UnityEngine;
 
-[System.Serializable]
+[Serializable]
+public class DataList
+{
+    public CreateData[] lists;
+}
+
+[Serializable]
 public class CreateData
 {
     public int id;
@@ -11,22 +18,27 @@ public class CreateData
     public float rot_y;
 }
 
-[System.Serializable]
-public class DataList {
-    public CreateData[] data;
-}
-
-
-
 public class PrefabCreater : MonoBehaviour
 {
-    public static void CreateMultiplePrefabs(string jsonName, GameObject[] prefabs) {
+    public static void CreateMultiplePrefabs(string jsonName, GameObject[] prefabs, GameObject parent) {
         string _inputString = Resources.Load<TextAsset>(jsonName).ToString();
         DataList _dataList = JsonUtility.FromJson<DataList>(_inputString);
 
-        foreach (CreateData data in _dataList.data)
+        if (parent != null)
         {
-            Instantiate(prefabs[data.id], new Vector3(data.pos_x, data.pos_y, data.pos_z), Quaternion.AngleAxis(data.rot_y, Vector3.up));
+            GameObject _obj;
+            foreach (CreateData data in _dataList.lists)
+            {
+                _obj = Instantiate(prefabs[data.kind], new Vector3(data.pos_x, data.pos_y, data.pos_z), Quaternion.AngleAxis(data.rot_y, Vector3.up));
+                _obj.transform.parent = parent.transform;
+            }
+        }
+        else
+        {
+            foreach (CreateData data in _dataList.lists)
+            {
+                Instantiate(prefabs[data.kind], new Vector3(data.pos_x, data.pos_y, data.pos_z), Quaternion.AngleAxis(data.rot_y, Vector3.up));
+            }
         }
     }
 }
