bf06744d8b1a84d64375934329b9b5d984b8fa6c
MSI\STzin <s.t.zint@gmail.com>
2022-11-30 15:24:44 +0900

    [調整]パラメータ読み取りクラス

--- Assets/Scripts/Saito_Class/WGameProgress/ParametersSet.cs
+++ Assets/Scripts/Saito_Class/WGameProgress/ParametersSet.cs
@@ -1,46 +1,60 @@
+using System;
 using UnityEngine;
 
-public class Parameters {
-    public float player_pos_x;
-    public float player_pos_y;
-    public float player_pos_z;
-    public float sbmarine_pos_x;
-    public float sbmarine_pos_y;
-    public float sbmarine_pos_z;
+[Serializable]
+public class PARAM
+{
+    public Parameter param;
 }
 
-//public class Params
-//{
-//    public players players;
-//    public submarines submarines;
-//}
+[Serializable]
+public class Paramater
+{
+    public PLAYER player;
+    public SUBMARINE submaline;
+}
 
-//public class players
-//{
-//    public float pos_x;
-//    public float pos_y;
-//    public float pos_z;
-//}
+[Serializable]
+public class FIELD
+{
+    public float x_clip;
+    public float y_clip;
+    public float z_clip;
+}
 
-//public class submarines
-//{
-//    public float pos_x;
-//    public float pos_y;
-//    public float pos_z;
-//}
+[Serializable]
+public class PLAYER
+{
+    public float pos_x;
+    public float pos_y;
+    public float pos_z;
+    public float move_speed;
+    public float move_boost;
+    public float oxy_max;
+    public float oxy_cost_boost;
+    public float oxy_cost;
+}
+
+[Serializable]
+public class SUBMARINE
+{
+    public float pos_x;
+    public float pos_y;
+    public float pos_z;
+}
 
 public class ParametersSet
 {
-    Parameters parameters_;
+    Paramater param;
 
     public void SetParameter()
     {
-        string inputString = Resources.Load<TextAsset>("InputData/ParameterData").ToString();
-        parameters_ = JsonUtility.FromJson<Parameters>(inputString);
+        string inputString = Resources.Load<TextAsset>("InputData/TestParameterData").ToString();
+        param = JsonUtility.FromJson<Paramater>(inputString);
     }
 
-    public Parameters GetParameter()
+    public Paramater GetParameter()
     {
-        return parameters_;
+        return param;
     }
 }
