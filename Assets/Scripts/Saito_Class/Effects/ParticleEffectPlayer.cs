5c479dede93c48e7d2f3fb494438ee2f936f6d32
MSI\STzin <s.t.zint@gmail.com>
2022-12-05 15:37:33 +0900

    [追加]フェードイン・アウトのアセット

---
+++ Assets/Scripts/Saito_Class/Effects/ParticleEffectPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectPlayer : MonoBehaviour
+{
+    ParticleSystem ps_ = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps_ = GetComponent<ParticleSystem>();
+    }
+
+    /// <summary>
+    /// 自身のパーティクルを再生する
+    /// </summary>
+    public void PlayOneShot()
+    {
+        if (ps_.isStopped)
+        {
+            ps_.Play();
+            StartCoroutine(IsFine());
+        }
+    }
+
+    IEnumerator IsFine()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (ps_.isStopped)
+            {
+                yield break;
+            }
+        }
+    }
+}
