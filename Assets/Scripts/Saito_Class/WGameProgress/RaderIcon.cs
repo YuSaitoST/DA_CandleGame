9441802f1bf47732c162c27198c81d0b496346a3
MSI\STzin <s.t.zint@gmail.com>
2022-12-26 23:36:44 +0900

    [調整]RaderIconの処理を追加

--- Assets/Scripts/Saito_Class/WGameProgress/RaderIcon.cs
+++ Assets/Scripts/Saito_Class/WGameProgress/RaderIcon.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class RaderIcon : MonoBehaviour
 {
+    [SerializeField, Tooltip("アイコン")] GameObject pref_icon_ = null;
+    GameObject icon_;
+
+    private void Start()
+    {
+        icon_ = Instantiate(pref_icon_, new Vector3(0.0f, 10.0f, 0.0f), Quaternion.identity);
+        icon_.transform.parent = transform;
+    }
+
     /// <summary>
     /// 発見したら、アイコンを削除する
     /// </summary>
     public void Detectioned()
     {
-        Destroy(gameObject);
+        Destroy(icon_);
     }
 }
