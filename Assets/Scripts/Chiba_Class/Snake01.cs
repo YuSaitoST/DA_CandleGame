75861093ad924fc877458dba704f4f5e5175a319
chibatomo <tomokichiba0803@gmail.com>
2023-01-27 15:46:53 +0900

    [調整]海蛇（頑固）が爆弾の強さによってスタンする時間が変わるようにした

--- Assets/Scripts/Chiba_Class/Snake01.cs
+++ Assets/Scripts/Chiba_Class/Snake01.cs
@@ -6,7 +6,17 @@ public class Snake01 : MonoBehaviour
 {
     [Header("ƒXƒ^ƒ“ŽžŠÔ")]
     [SerializeField]
-    private float stan_time_ = 5.0f;
+    private float[] stan_time_ =
+        {
+          5.0f,
+          6.0f,
+          7.0f,
+          8.0f,
+          9.0f,
+          10.0f
+           
+
+        };
 
     [SerializeField]
     private Animator animator_ = null;
@@ -41,7 +51,7 @@ public class Snake01 : MonoBehaviour
 
             if (collision.gameObject.tag == "Player")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(0));
             }
         }
     }
@@ -52,32 +62,32 @@ public class Snake01 : MonoBehaviour
         {
             if (other.gameObject.tag == "OxyBomb00")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(0));
             }
 
             if (other.gameObject.tag == "OxyBomb01")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(1));
             }
 
             if (other.gameObject.tag == "OxyBomb02")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(2));
             }
 
             if (other.gameObject.tag == "OxyBomb03")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(3));
             }
 
             if (other.gameObject.tag == "OxyBomb04")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(4));
             }
 
             if (other.gameObject.tag == "OxyBomb05")
             {
-                StartCoroutine(OnStan());
+                StartCoroutine(OnStan(5));
             }
         }
     }
@@ -88,13 +98,13 @@ public class Snake01 : MonoBehaviour
         
     //}
 
-    IEnumerator OnStan()
+    IEnumerator OnStan(int _bomb)
     {
         Debug.Log("‚Ô‚Â‚©‚Á‚½");
         animator_.SetBool("isDamage", true);
         stan_flg_ = true;
 
-        yield return new WaitForSeconds(stan_time_);
+        yield return new WaitForSeconds(_bomb);
         animator_.SetBool("isDamage", false);
         stan_flg_ = false;
     }
